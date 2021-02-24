288e6ee247ba913c13dd436479c9425d2454f517
evertonnatan <evertonnatandl@outlook.com>
2021-02-23 21:44:47 -0300

    Dando continuidade a aula 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { Button} from './components/Button'
+import './styles/global.css';
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Button color="red">Botão 1</Button>
+      <Button color="blue">Botão 2</Button>
+      <Button color="green">Botão 3</Button>
+    </div>
   );
 }
 
