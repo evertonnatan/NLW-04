3d0e1a1091b28a5b91b4fec8aea5a33c5b20452b
evertonnatan <evertonnatandl@outlook.com>
2021-02-26 09:05:00 -0300

    Countdown finalizado

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -5,6 +5,7 @@ let countdownTimeout: NodeJS.Timeout
 export function Countdown() {
     const [time, setTime] = useState(0.1 * 60)
     const [isActive, setIsActive] = useState(false)
+    const [hasFinished, setHasFinished] = useState(false)
 
     const minutes = Math.floor(time / 60)
     const seconds = time % 60
@@ -25,6 +26,9 @@ export function Countdown() {
             countdownTimeout = setTimeout(() => {
                 setTime(time -1)
             }, 1000)
+        } else if (isActive && time === 0) {
+            setHasFinished(true)
+            setIsActive(false)
         }
     }, [isActive, time])
 
@@ -44,24 +48,35 @@ export function Countdown() {
             </div>
         </div>
 
-        { isActive ? (
-        <button 
-            type="button" 
-            className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
-            onClick={resetCountdown}
-            >
-            Abandonar ciclo            
-        </button>
-
-        ) : (
-        <button 
-            type="button" 
+        {hasFinished ? (
+            <button
+            disabled
             className={styles.countdownButton}
-            onClick={startCountdown}
             >
-            Iniciar um ciclo            
-        </button>
-        )}
+            Ciclo encerrado
+            </button>
+        ) : (
+            <>
+                { isActive ? (
+                <button 
+                type="button" 
+                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                onClick={resetCountdown}
+                >
+                Abandonar ciclo            
+                </button>
+
+                ) : (
+                <button 
+                type="button" 
+                className={styles.countdownButton}
+                onClick={startCountdown}
+                >
+                Iniciar um ciclo            
+                </button>
+                )}
+            </>
+        )}        
         </div>
     );
 }
