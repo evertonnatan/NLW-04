db23d8a7eee5a2d2b33a9b14907b5ee5587ccfc3
evertonnatan <evertonnatandl@outlook.com>
2021-02-27 17:01:21 -0300

    Finalizando a aula 03

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -9,6 +9,7 @@ interface Challenge {
 interface ChallengesContextData {
     level: number;
     currentExperience: number;
+    experienceToNextLevel: number;
     challengesCompleted: number;
     activeChallenge: Challenge;
     levelUp: () => void;
@@ -22,10 +23,12 @@ interface ChallengesProviderProps {
 export const ChallengeContext = createContext({} as ChallengesContextData)
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1)
-  const [currentExperience, setCurrentExperience] = useState(0)
+  const [currentExperience, setCurrentExperience] = useState(35)
   const [challengesCompleted, setChallengesCompleted] = useState(0)
 
   const [activeChallenge, setActiveChallenge] = useState(null)
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
   function levelUp() {
     setLevel(level + 1)
@@ -44,6 +47,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         <ChallengeContext.Provider value={{ 
             level, 
             currentExperience, 
+            experienceToNextLevel,
             challengesCompleted, 
             levelUp,
             startNewChallenge,
