bf3c2fd9362c70fe548fe51d370c9c2c8f4a6caa
evertonnatan <evertonnatandl@outlook.com>
2021-03-01 22:15:38 -0300

    Checkpoint da aula

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
@@ -8,8 +9,10 @@ import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from '../styles/pages/Home.module.css';
 import { CountdownProvider } from '../contexts/CountdownContext';
+import { createGzip } from 'zlib';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props)
   return (
     <div className={styles.container}>
       <Head>
@@ -35,3 +38,15 @@ export default function Home() {
   )
 }
 
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  
+  return {
+    props: {
+    level,
+    currentExperience,
+    challengesCompleted,
+    }
+  }
+}
